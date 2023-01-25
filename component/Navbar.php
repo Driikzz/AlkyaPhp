fd12b0aabff186a16134846905119cc8a8fdb08f
Pacome <pacomedanilo503@gmail.com>
2023-01-25 17:25:48 +0100

    ttt

---
+++ component/Navbar.php
@@ -0,0 +1,31 @@
+<!-- <?php
+  require_once "php/config.php";
+   $sqlProject = "SELECT id,first_title_project FROM projects"; 
+   $pre = $pdo->prepare($sqlProject); 
+   $pre->execute();
+   $idProject = $pre->fetchAll(PDO::FETCH_ASSOC);
+?> -->
+
+<header class="">
+            <div class="nav-img">
+                <img src="img/site-principal/bande nue bleu alkya 2.png" alt="">
+                <img src="img/site-principal/bande nue bleu alkya 3.png" alt="">
+            </div>
+            <div class=""> 
+                <nav class="row">
+                    <div class="col s1">
+                        <img src="img/site-principal/Logo Colored PNG 1.png" alt="">
+                    </div>
+                    <div class="navbar-link col s5" >
+                        <ul>
+                            <a class="black-text" href=""><li>News</li></a>
+                            <a class="black-text" href=""><li>Teams</li></a>
+                            <a class="black-text" href=""><li>About us</li></a>
+                            <a class="black-text" href=""><li>Shop</li></a>
+                        </ul>
+                    </div>
+                    <div class="col s4"></div>
+                </nav>
+                <div class="center-align bar-nav" ></div>
+            </div>
+        </header>
